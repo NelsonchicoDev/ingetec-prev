e302e2e10a2c2ad5d7a5a1f69e9f16d42ff87564
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-12-09 23:24:02 +0600

    work page error resolved

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -7,7 +7,6 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import React, { useState } from "react";
 import projectOne from "@/images/projectOne.png";
 import projectTwo from "@/images/projectTwo.png";
 import projectThree from "@/images/projectThree.png";
@@ -61,23 +60,10 @@ const projects = [
 ];
 
 const WorkPage = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const handleSlideChange = (index: number) => {
-    setCurrentIndex(index);
-  };
   return (
     <div className="py-6 md:py-12">
       <PageLayout>
-        <Carousel
-          opts={{ align: "start", loop: true }}
-          className="w-full"
-          onSelect={() => {
-            const index = 0;
-            if (typeof index === "number") {
-              handleSlideChange(index);
-            }
-          }}
-        >
+        <Carousel opts={{ align: "start", loop: true }} className="w-full">
           <CarouselContent>
             {projects?.map((project) => (
               <CarouselItem key={project.id}>
@@ -156,7 +142,7 @@ const WorkPage = () => {
                       </div>
                       {/* image */}
                       <div className="w-full md:w-1/2 order-1 md:order-2">
-                        <div className="relative h-64 md:h-96 bg-gray-700 rounded-lg overflow-hidden">
+                        <div className="relative h-72 sm:h-96 bg-gray-700 rounded-lg overflow-hidden">
                           <Image
                             src={project?.image}
                             alt={project.title}
