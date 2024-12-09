b956f18ab5f487040157c8548123c69650d4006b
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-12-09 23:32:03 +0600

    getform link removed

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -65,7 +65,7 @@ const ContactForm = () => {
       form.append("Service", formData.Service);
       form.append("DateTime", currentDateTime);
 
-      const response = await fetch("https://getform.io/f/akkkreqa", {
+      const response = await fetch("", {
         method: "POST",
         body: form,
       });
